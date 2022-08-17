92f331dce28b5e8be5f66c4b8f1d7def2dc364fb
Nastya <kartashovaanastasia17@gmail.com>
2022-08-17 23:36:40 +0300

    This is generator

---
+++ task_1.py
@@ -0,0 +1,18 @@
+'''
+Функция реализует генератор геометрической прогрессии и значение stop (до какого числа генерировать)
+'''
+def generator(value, denominator,stop):
+    value = int(value)
+    denominator = int(denominator)
+    n = 2
+    while value < stop:
+        value = value * (denominator ** (n-1))
+        yield value
+        n + 1
+
+
+for x in generator(1,2,100):
+    print('Значение поменялось на: ', x)
+
+
+
